cb74f4f8c9aeae01924d69226e56ab0cb390cfea
IvanRavic88 <ravic.ivan88@gmail.com>
2022-12-29 09:54:49 +0100

    webpack and typescript

---
+++ src/components/base-component.ts
@@ -0,0 +1,43 @@
+//Component base Class
+
+export default abstract class Component<
+  T extends HTMLElement,
+  U extends HTMLElement
+> {
+  templateElement: HTMLTemplateElement;
+  hostElement: T;
+  element: U;
+
+  constructor(
+    templateId: string,
+    hostElementId: string,
+    insertAtStart: boolean,
+    newElementID?: string
+  ) {
+    this.templateElement = document.getElementById(
+      templateId
+    ) as HTMLTemplateElement;
+    this.hostElement = document.getElementById(hostElementId) as T;
+
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true
+    );
+    this.element = importedNode.firstElementChild as U;
+
+    if (newElementID) {
+      this.element.id = newElementID;
+    }
+    this.attach(insertAtStart);
+  }
+
+  private attach(insertAtStart: boolean) {
+    this.hostElement.insertAdjacentElement(
+      insertAtStart ? "afterbegin" : "afterend",
+      this.element
+    );
+  }
+
+  abstract configure(): void;
+  abstract renderContent(): void;
+}
